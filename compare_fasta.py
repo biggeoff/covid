6061701d019039c67237b498077fc3314cbc5827
Geoff Woodward <geoffw@nbsvr484.northbristol.local>
2021-03-11 10:46:23 +0000

    Plated with changed in compare_fasta.py but differences between sequences too big - use NextClade for manual comparisons instead

--- compare_fasta.py
+++ compare_fasta.py
@@ -2,6 +2,7 @@ from Bio import SeqIO
 import pandas as pd
 import argparse
 import os
+import glob
 
 
 def loadMetadata():
@@ -26,7 +27,9 @@ def compareSeqIgnoreN(seq1, seq2):
     mismatches=0
     n=0
     for i, base in enumerate(seq1):
-        if seq1[i] == "N" or seq2[i] == "N":
+        if i > 29901:
+            continue
+        elif seq1[i] == "N" or seq2[i] == "N":
             n += 1
             continue
         elif seq1[i] == seq2[i]:
@@ -61,3 +64,109 @@ if __name__ == "__main__":
     meta['tail_untail_identity'] = meta.apply(compareAll, 1)
 
 
+'''
+#######################
+# 4 mismatches
+#######################
+
+#BRIS-25CE90	4-20V00335709  - swap
+#BRIS-25CF06	4-20V00335821 - swap
+
+#BRIS-1855B5B	20V60165216
+#BRIS-1856659    20V60170512
+#BRIS-25CF06     20V00335821
+#BRIS-25AC4D     20V60171170
+
+
+#####################################################
+# failed because they're too different!!! bad sign!
+#####################################################
+cogs=['BRIS-1855B5B', 'BRIS-1856659','BRIS-25CF06', 'BRIS-25AC4D']
+wins=['20V60165216','20V60170512','20V00335821','20V60171170']
+
+root='/mnt/NGS_DATA_at_Bristol/COVID'
+test_n='/tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus'
+truth_n='/verification/96plate_validation/'
+for i, cog in enumerate(cogs):
+    truth=glob.glob(root+truth_n+cogs[i]+"*")
+    tr=loadFasta(truth[0])
+    test_reps=glob.glob(root+test_n+"/*"+wins[i]+"*")
+    for testr in test_reps:
+        print("Testing:\n \t{} [{}] and \n\t{} [{}]".format(
+            testr.split('/')[-1],
+            len(te[0].seq),
+            truth[0].split('/')[-1],
+            len(tr[0].seq)
+            )
+        )
+        te=loadFasta(testr)
+        compareSeqIgnoreN(tr[0], te[0])
+        print("=======================")
+'''
+
+'''
+########################################
+# Needle far too slow....
+########################################
+from Bio.Emboss.Applications import NeedleCommandline
+needle_align_code(tr[0].seq, te[0].seq)
+#extremely slow
+def needle_align_code(query_seq, target_seq):
+    needle_cline = NeedleCommandline(asequence="asis:" + query_seq,
+                                     bsequence="asis:" + target_seq,
+                                     aformat="simple",
+                                     gapopen=10,
+                                     gapextend=0.5,
+                                     outfile='stdout'
+                                     )
+    out_data, err = needle_cline()
+    out_split = out_data.split("\n")
+    p = re.compile("\((.*)\)")
+    return p.search(out_split[25]).group(1).replace("%", "")
+
+
+
+
+############################
+# Pairwise2 doesn't ignore Ns
+############################
+from Bio import pairwise2
+global_align = pairwise2.align.globalxx(tr[0].seq, te[0].seq)
+
+'''
+
+'''
+cogs=['BRIS-1855B5B', 'BRIS-1856659',, 'BRIS-25AC4D']
+wins=['20V60165216','20V60170512','','20V60171170']
+sudo bash
+cat verification/96plate_validation/BRIS-1855B5B* \
+verification/96plate_validation/BRIS-1856659* \ 
+verification/96plate_validation/BRIS-25CF06* \
+verification/96plate_validation/BRIS-25AC4D* > expected.fa
+
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V60165216* > 20V60165216.all.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V60170512* > 20V60170512.all.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V00335821* > 20V00335821.all.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V60171170* > 20V60171170.all.fa
+
+cat *.fa > follow_up.fa
+
+#SWAPPED:
+cat verification/96plate_validation/BRIS-25CF06* > sample_swap_investigation.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V00335709* >> sample_swap_investigation.fa
+cat verification/96plate_validation/BRIS-25CE90* >> sample_swap_investigation.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V00335821* >> sample_swap_investigation.fa
+
+#Other 4:
+cat verification/96plate_validation/BRIS-1855B5B* > BRIS-1855B5B_investigation.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V60165216* >> BRIS-1855B5B_investigation.fa
+cat verification/96plate_validation/BRIS-1856659* > BRIS-1856659_investigation.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V60170512* >> BRIS-1856659_investigation.fa
+cat verification/96plate_validation/BRIS-1855845* > BRIS-1855845_investigation.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V00299373* >> BRIS-1855845_investigation.fa
+cat verification/96plate_validation/BRIS-25AC4D* > BRIS-25AC4D_investigation.fa
+cat tailed_runs_2-5/ncov2019-arctic-nf/ncovIllumina_sequenceAnalysis_makeConsensus/*20V60171170* >> BRIS-25AC4D_investigation.fa
+
+uploaded to: https://clades.nextstrain.org/
+
+'''
