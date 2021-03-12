0f45db1b922d5aa3f8831149ca16d8aa44dd2421
Geoff Woodward <geoffw@nbsvr484.northbristol.local>
2021-03-12 14:43:48 +0000

    placeholder script added: platemap2winpath.py - not a priority

---
+++ platemap2winpath.py
@@ -0,0 +1,8 @@
+
+# cronjob - find file parse, mv to samplenet, 
+#every sample twice:
+
+1) "Target Name" = "COGRF" with "Ct"=COG-ID
+2) "Target name" = "COGST" with "Ct"="EXTRACT"
+
+#upload to the same place as other script.
