89f2d0558e76d181ece5848aa779e312b7519319
Geoff Woodward <geoffw@nbsvr484.northbristol.local>
2021-04-07 17:43:31 +0100

    Updated CT_value_ivnestigation to use argparse - and reused for the second validation run

--- CT_value_investigation.py
+++ CT_value_investigation.py
@@ -3,14 +3,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import argparse
 
 
-def load():
-      df=pd.read_csv('20210309_pic_pang_nextc.qc.csv')
-      ct=pd.read_csv('20210903_ct.csv')
+def load(worklist):
+      df=pd.read_csv(worklist+'_pic_pang_nextc_covert.qc.csv')
+      ct=pd.read_csv(worklist+'_ct.csv')
       ss=pd.read_csv('../SampleSheet.csv', skiprows=19)
       all = pd.merge(left=ss, right=df, left_on='Sample_ID', right_on='case')
-      all = pd.merge(left=all, right=ct, left_on='Description', right_on='case')
+      all = pd.merge(left=all, right=ct, left_on='case', right_on='case')
       all['colour']="Red"
       all.loc[all.qc_pass==True, 'colour']="Green"
       return all
@@ -38,15 +39,23 @@ def scatterPlotLinReg(data, x_col, y_col, plot_name):
       plt.close()
 
 
-os.chdir('/largedata/share/MiSeqOutput2/210319_M03605_0243_000000000-JJDPJ/ncov2019-arctic-nf/')
 
-all = load()
-all = undeterminedToZero(all)
-all['mean_ct'] = all['Beta-CoV-E Gene'] / 2 + all['SARS-CoV-2 S Gene'] / 2
+if __name__ == "__main__":
 
-scatterPlotLinReg(all, "MEDIAN_INSERT_SIZE", "mean_ct", "Insert_size_vs_mean_cT.png") # mean insert size VS mean cT
-scatterPlotLinReg(all, "READ_PAIRS", "mean_ct", "Reads_vs_mean_cT.png") # mean insert size VS mean cT
-scatterPlotLinReg(all, "READ_PAIRS", "SARS-CoV-2 S Gene", "Reads_vs_S_gene_cT.png") # mean insert size VS mean cT
-scatterPlotLinReg(all, "READ_PAIRS", "Beta-CoV-E Gene", "Reads_vs_E_gene_cT.png") # mean insert size VS mean cT
+      parser = argparse.ArgumentParser()
+      parser.add_argument("-a", "--arctic", nargs=1, type=str, help="full path to artice pipeline output folder", required=True)
+      parser.add_argument("-w", "--worklist", nargs=1, type=str, help="Name of the run/worklist", required=True)
+      args = parser.parse_args()
 
-pd.to_csv(all, "20210309_pic_pang_nextc_CT.qc.csv")
+      os.chdir(args.arctic[0])
+
+      all = load(args.worklist[0])
+      all = undeterminedTo42(all)
+      all['mean_ct'] = all['Beta-CoV-E Gene'] / 2 + all['SARS-CoV-2 S Gene'] / 2
+
+      scatterPlotLinReg(all, "MEDIAN_INSERT_SIZE", "mean_ct", "Insert_size_vs_mean_cT.png") # mean insert size VS mean cT
+      scatterPlotLinReg(all, "READ_PAIRS", "mean_ct", "Reads_vs_mean_cT.png") # mean insert size VS mean cT
+      scatterPlotLinReg(all, "READ_PAIRS", "SARS-CoV-2 S Gene", "Reads_vs_S_gene_cT.png") # mean insert size VS mean cT
+      scatterPlotLinReg(all, "READ_PAIRS", "Beta-CoV-E Gene", "Reads_vs_E_gene_cT.png") # mean insert size VS mean cT
+
+      all.to_csv(args.worklist[0]+"_pic_pang_nextc_CT.qc.csv")
