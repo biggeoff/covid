df431b84598aba6545afbf37d54c0a4a9cd913f0
Geoff Woodward <geoffw@nbsvr484.northbristol.local>
2021-04-19 18:11:12 +0100

    Added wait() to popen and a delay between API calls

--- ocarina_API.py
+++ ocarina_API.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 from glob import glob
 import argparse
+import time
 
 
 def putLibrary(worklist, cog_id, std, err):
@@ -12,10 +13,11 @@ def putLibrary(worklist, cog_id, std, err):
     cmd+="--library-name PHESW-LIB-"+worklist+" "
     cmd+="--library-seq-kit 'Illumina MiSeq v3' "
     cmd+="--library-seq-protocol 'MiSeq 300 Cycle'"
-    print(cmd+"\n")
+    std.write(cmd+"\n")
     std.flush()
     err.flush()
     proc = subprocess.Popen(cmd, shell=True, stdout=std, stderr=err)
+    proc.wait()
     return 
 
 
@@ -28,20 +30,21 @@ def putSequencing(worklist, run, std, err, starttime='', endtime='', flowcell=''
     cmd+= "--bioinfo-pipe-name 'ARTIC Pipeline (iVar)' "
     cmd+= "--bioinfo-pipe-version 1.3.0 " 
     cmd+= "--flowcell-type v3"
-    print(cmd+"\n")
+    std.write(cmd+"\n")
     std.flush()
     err.flush()
     proc = subprocess.Popen(cmd, shell=True, stdout=std, stderr=err)
+    proc.wait()
     #result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     #std.write(result.stdout)
     #err.write(result.stderr)
 
 
-def findCases(arctic_dir, worklist):
+def findCases(arctic_dir, subdir):
     """
     find all of the 
     """
-    fullpaths = glob(arctic_dir+'/qc_pass_climb_upload/'+worklist+'/*/')
+    fullpaths = glob(arctic_dir+'/qc_pass_climb_upload/'+subdir+'/*/')
     cases = [f.strip('/').split("/")[-1] for f in fullpaths] # remove filepath
     parsed = [c for c in cases if not c.startswith("Pos")] # remove postive controls
     return cases
@@ -59,18 +62,18 @@ def findCases(arctic_dir, worklist):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--arctic", nargs=1, type=str, help="full path to arctic output folder", required=True)
+    parser.add_argument("-d", "--subdir", nargs=1, type=str, help="subdir to be uploaded to climb (miseq run)", required=True)
     parser.add_argument("-w", "--worklist", nargs=1, type=str, help="Name of the run/worklist", required=True)
     args = parser.parse_args()
     
-    run = args.arctic[0].strip('/').split('/')[-2]
-
     log = open(args.arctic[0]+'/ocarina.log', 'a')
     err = open(args.arctic[0]+'/ocarina.err', 'a')
-    cases = findCases(args.arctic[0], args.worklist[0])
+    cases = findCases(args.arctic[0], args.subdir[0])
     for case in cases:
         putLibrary(args.worklist[0], case, log, err)
+        time.sleep(2)
     
-    putSequencing(worklist, run, log, err)
+    putSequencing(args.worklist[0], args.subdir[0], log, err)
     log.close()
     err.close()
 
