e830fcd342e8eb8ab6cf35276926875e4cbdebdb
Geoff Woodward <geoffw@nbsvr484.northbristol.local>
2021-05-04 10:21:48 +0100

    Added settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+BCL2FASTQ2_CONTAINER = "geoffw/centos_bcl2fastq2"
+BCL2FASTQ_EXE = "bcl2fastq"
+BCL2FASTQ_VERSION = "v2.16.0.10"
+
+ARCTIC_PATH = "/home/geoffw/sandpit/ncov2019-artic-nf"
+ARCTIC_CACHE = "/home/geoffw/miniconda3/envs/artic-ncov2019-illumina/"
+ARCTIC_BED = "/fastdata/ncov2019-arctic/nCoV-2019/V3/nCoV-2019.bed"
+ARCTIC_REF = "nCoV-2019.reference.fasta"
+ARCTIC_RESOURCE = "/fastdata/ncov2019-arctic/nCoV-2019/V3/"
+
+NEXTCLADE_DOCKER = "nextstrain/nextclade"
+PANGOLIN_DOCKER = "staphb/pangolin:latest"
+
+PICARD_DOCKER = "geoffw/picard1.67"
+PICARD_JAR = "/picard-tools-1.67/CollectInsertSizeMetrics.jar"
