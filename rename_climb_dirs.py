3149b90cf929d41b960c4c7c8f1f1fecfef73149
Geoff Woodward <geoffw@nbsvr484.northbristol.local>
2021-05-04 10:21:15 +0100

    renameclimb_dirs.py: now renames subdir to illumina run as well as indivudlal patients

--- rename_climb_dirs.py
+++ rename_climb_dirs.py
@@ -3,8 +3,15 @@ import argparse
 from glob import glob
 
 
-def getCLIMBsubdirs(arctic_dir, worklist):
-    return glob(arctic_dir+'/qc_pass_climb_upload/'+worklist+'/*/')
+def renameSubdir(arctic_dir, worklist, illumina):
+    if os.path.exists(arctic_dir+'/qc_pass_climb_upload/'+worklist):
+        print ("\nRenaming: qc_pass_climb_upload/{} to {}\n".format(worklist, illumina))
+        os.rename(arctic_dir+'/qc_pass_climb_upload/'+worklist, 
+            arctic_dir+'/qc_pass_climb_upload/'+illumina)
+
+
+def getCLIMBsubdirs(arctic_dir, illumina):
+    return glob(arctic_dir+'/qc_pass_climb_upload/'+illumina+'/*/')
 
 
 def removeAfterDelim(subdirs, delim='_', count=1):
@@ -26,12 +33,11 @@ if __name__ == "__main__":
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--arctic", nargs=1, type=str, help="full path to arctic output folder", required=True)
-    parser.add_argument("-w", "--worklist", nargs=1, type=str, help="Name of the run/worklist", required=True)
+    parser.add_argument("-w", "--worklist", nargs=1, type=str, help="Name of the worklist", required=True)
+    parser.add_argument("-r", "--run", nargs=1, type=str, help="Name of the run", required=True)
     args = parser.parse_args()
 
-    subdirs = getCLIMBsubdirs(args.arctic[0], args.worklist[0])
-    removeAfterDelim(subdirs, delim='_', count=2)
-
-    subdirs = getCLIMBsubdirs(args.arctic[0], args.worklist[0])
+    renameSubdir(args.arctic[0], args.worklist[0], args.run[0])    
+    subdirs = getCLIMBsubdirs(args.arctic[0], args.run[0])
     removeAfterDelim(subdirs, delim='-', count=1)
 
